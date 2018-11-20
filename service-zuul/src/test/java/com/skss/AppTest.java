be4af43c19c457b07e1bde49dd30f18148551e9f
corn <cornmountain@163.com>
2018-11-20 13:34:33 +0800

    config eureka

--- service-zuul/src/test/java/com/skss/AppTest.java
+++ service-zuul/src/test/java/com/skss/AppTest.java
@@ -1,8 +1,14 @@
 package com.skss;
 
-import static org.junit.Assert.assertTrue;
-
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClients;
 import org.junit.Test;
+
+import java.net.URI;
 
 /**
  * Unit test for simple App.
@@ -15,6 +21,31 @@ public class AppTest
     @Test
     public void shouldAnswerWithTrue()
     {
-        assertTrue( true );
+        //测试公司的API接口，将json当做一个字符串传入httppost的请求体
+               String result = null;
+               HttpClient client = HttpClients.createDefault();
+               URIBuilder builder = new URIBuilder();
+               URI uri = null;
+               try {
+                   // http://localhost:8881/actuator/bus-refresh
+                   uri = builder.setScheme("http")
+                             .setHost("localhost").setPort(8881)
+                              .setPath("/actuator/bus-refresh")
+                              .build();
+
+                    HttpPost post = new HttpPost(uri);
+                    //设置请求头
+                    post.setHeader("Content-Type", "application/json");
+                    String body = "{\"Key\": \"\",\"Secret\": \"\"}";
+                    //设置请求体
+                    post.setEntity(new StringEntity(body));
+                    //获取返回信息
+                    HttpResponse response = client.execute(post);
+                    result = response.toString();
+                } catch (Exception e) {
+                    System.out.println("接口请求失败"+e.getStackTrace());
+                }
+                System.out.println(result);
+
     }
 }
